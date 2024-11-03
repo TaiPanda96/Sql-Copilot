cde3a786afbedcb8e64de31fd80ad2b2934de0a7
TaiShan  Lin <taishanlin@TaiShans-MBP.ht.home>
2024-11-03 11:06:29 -0500

    latest

--- next.config.mjs
+++ next.config.mjs
@@ -8,13 +8,6 @@ const nextConfig = {
         }
 
         return config
-    },
-    env: {
-        KINDE_SITE_URL: process.env.KINDE_SITE_URL ?? `https://${process.env.VERCEL_URL}`,
-        KINDE_POST_LOGOUT_REDIRECT_URL:
-            process.env.KINDE_POST_LOGOUT_REDIRECT_URL ?? `https://${process.env.VERCEL_URL}`,
-        KINDE_POST_LOGIN_REDIRECT_URL:
-            process.env.KINDE_POST_LOGIN_REDIRECT_URL ?? `https://${process.env.VERCEL_URL}/api/auth/success`,
     }
 };
 
