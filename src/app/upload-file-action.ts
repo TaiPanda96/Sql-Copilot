1c69da0a65d99504198c114d6e0dbd1bce8d8ba7
TaiShan  Lin <taishanlin@TaiShans-MacBook-Pro.local>
2025-01-19 22:36:04 -0500

    added file create IO

--- src/app/upload-file-action.ts
+++ src/app/upload-file-action.ts
@@ -3,13 +3,15 @@
 import { validateActionInput } from "@sql-copilot/lib/components/use-form-action";
 import { z } from "zod";
 import { uploadFileSchema } from "./upload-file-input";
+import { createContext } from "@sql-copilot/lib/create-context";
 
 export async function uploadFileAction(
   input: z.input<typeof uploadFileSchema>
-): Promise<{ success: boolean }> {
+): Promise<{ success: boolean; fileUrl: string }> {
+  const ctx = await createContext(["prisma"]);
   const validation = validateActionInput(input, uploadFileSchema);
   if (!validation.success) {
-    return { success: false };
+    return { success: false, fileUrl: "" };
   }
   const { url, fileName, story } = validation.data;
 
@@ -21,14 +23,23 @@ export async function uploadFileAction(
     // Process the file here
     console.log("Processing file:", { url, fileName, story });
 
+    const newFile = await ctx.prisma.files.create({
+      data: {
+        name: outputFileName,
+        url,
+      },
+    });
+
     // IO to store the story and url in the database
     return {
       success: true,
+      fileUrl: newFile.url,
     };
   } catch (error) {
     console.error("Error processing files:", error);
     return {
       success: false,
+      fileUrl: "",
     };
   }
 }
